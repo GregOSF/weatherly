640a7b9985640b64d33e67bd25a38affd0e0fd73
GregOSF <gregory.onza@gmail.com>
2015-09-30 16:15:34 -0700

    Add if else stmts for temp images; get temps

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ angular.module('weatherly', [])
 		    // controller logic
 
 	}])
+
 	.controller('WeatherCtrl', ['$scope', '$http', function ($scope, $http) {
 			var unixTime = function convertTimestamp(timestamp) {
 			  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
@@ -15,7 +16,7 @@ angular.module('weatherly', [])
 					min = ('0' + d.getMinutes()).slice(-2),		// Add leading 0.
 					ampm = 'AM',
 					time;
-						
+					
 				if (hh > 12) {
 					h = hh - 12;
 					ampm = 'PM';
@@ -34,52 +35,62 @@ angular.module('weatherly', [])
 				  'Friday',
 				  'Saturday'
 				);
-
-				
 				var dayName = gsDayNames[d.getDay()];
 				console.log(dayName)
 				// ie: 2013-02-18, 8:35 AM	
 				time = yyyy + '-' + mm + '-' + dayName + ', ' + h + ':' + min + ' ' + ampm;
-					
 				return dayName;
 			};
-			
-
-
-			
+	
 			$scope.searchCity = function () {
 				$scope.cities = [];
 				console.log($scope.city);
 				var cityName = $scope.cityName;
-				var searchUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&mode=json';
+				var searchUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&mode=json&units=imperial';
 				console.log(searchUrl);
 
 				$http.get(searchUrl)
 					.then (function(response) {
 						console.log(response);
 						$scope.city=response.data;
+						var cityObj = $scope.city
 						console.log(response.data);
 						var cityId = response.data.city.id;
 						console.log(cityId);
 						$scope.cities.push(response.data.city.name);
 						console.log($scope.cities.length)
-
 						var datetime = $scope.city.list[0].dt;
+						console.log(datetime)
 						console.log(unixTime(datetime));
-						var temp = $scope.city.list[0].main.temp;
+						var temp = Math.ceil(cityObj.list[0].main.temp);
+						$scope.todayTemp = temp;
 						console.log(temp);
 						for (i = 0; i < 5; i++) { 
-									var temps = $scope.city.list[i].main.temp;
+									var temps = cityObj.list[i].main.temp;
 							    console.log(temps);
 						};
+						$scope.day=unixTime(datetime)
+						var forecastId = cityObj.list[0].weather[0].id;
+						console.log(forecastId);
 
-
-
-
-
-
-					})
-
+						var weatherImg = function getImage () {
+							if (forecastId >= 200 && forecastId <= 299) {
+						  	return "img/thunderstorm.png";						  	
+						  } else if (forecastId >= 300 && forecastId <= 399) {
+						  	return "img/drizzle.png";						  	
+						  } else if (forecastId >= 500 && forecastId <= 599) {
+						  	return "img/rain.png";
+						  } else if (forecastId >= 600 && forecastId <= 699) {
+						  	return "img/snow.png";						  	
+						  } else if (forecastId >= 700 && forecastId <= 799) {
+						  	return "img/atmosphere.png";						  	
+						  } else if (forecastId >= 800 && forecastId <= 899) {
+						  	return "img/clouds.png";						  	
+						  }
+						};
+						$scope.image = weatherImg();
+						console.log($scope.image)
+				});
 			}
 
 	}]);
